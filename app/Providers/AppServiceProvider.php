db229cd288d53b6ba552298b85e080071bb85043
Rubén Arcos <rubenarcos2@gmail.com>
2022-07-20 11:58:23 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         if (env('REDIRECT_HTTPS')) {
             $url->formatScheme('https://');
